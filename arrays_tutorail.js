91bb71833ba5142c5685d11b9dbfc5ab6064c847
webdevwise100 <138401950+webdevwise100@users.noreply.github.com>
2023-07-18 21:46:02 +0100

    Update arrays_tutorail.js

--- arrays_tutorail.js
+++ arrays_tutorail.js
@@ -1,166 +1,179 @@
-// 1. push(): Adds one or more elements to the end of an array and returns the new length of the array.
-const array1 = [1, 2, 3];
-const newLength = array1.push(4, 5);
-console.log("push() - array1:", array1); // [1, 2, 3, 4, 5]
-console.log("push() - newLength:", newLength); // 5
+// Modifications
 
-// 2. pop(): Removes the last element from an array and returns that element.
-const array2 = [1, 2, 3];
-const removedElement = array2.pop();
-console.log("pop() - array2:", array2); // [1, 2]
-console.log("pop() - removedElement:", removedElement); // 3
+// 1. push()
+// const array1 = [1, 2, 3, 4, 5];
+// const newLength = array1.push(6, 7);
+// console.log("array1 after push is = " + array1);
+// console.log("array1 newLength is = " + newLength);
 
-// 3. shift(): Removes the first element from an array and returns that element.
-const array3 = [1, 2, 3];
-const removedElement2 = array3.shift();
-console.log("shift() - array3:", array3); // [2, 3]
-console.log("shift() - removedElement:", removedElement2); // 1
+// 2. pop()
+// const array2 = [1, 2, 3, 4, 5];
+// const removedElement = array2.pop();
+// console.log("array2 after pop is =" + array2);
+// console.log("removedElement is = " + removedElement);
 
-// 4. unshift(): Adds one or more elements to the beginning of an array and returns the new length of the array.
-const array4 = [2, 3];
-const newLength2 = array4.unshift(0, 1);
-console.log("unshift() - array4:", array4); // [0, 1, 2, 3]
-console.log("unshift() - newLength:", newLength2); // 4
+// 3. shift()
+// const array3 = [1, 2, 3, 4, 5];
+// const removedElement = array3.shift();
+// console.log("array3 after shift is =" + array3);
+// console.log("removedElement is = " + removedElement);
 
-// 5. concat(): Combines two or more arrays and returns a new array.
-const array5 = [1, 2];
-const array6 = [3, 4];
-const newArray3 = array5.concat(array6);
-console.log("concat() - newArray3:", newArray3); // [1, 2, 3, 4]
+// 4. unshift()
+// const array4 = [1, 2, 3, 4, 5];
+// const newLength = array4.unshift(6, 7);
+// console.log("array4 after unshift is =" + array4);
+// console.log("newLength is = " + newLength);
 
-// 6. slice(): Extracts a portion of an array into a new array without modifying the original array.
-const array7 = [1, 2, 3, 4, 5];
-const newArray4 = array7.slice(1, 4);
-console.log("slice() - newArray4:", newArray4); // [2, 3, 4]
+// 5. slice()
+// const array5 = [1, 2, 3, 4, 5];
+// const newArray = array5.slice(1, 4);
+// console.log("array5 after slice is =" + array5);
+// console.log("newArray is = " + newArray);
 
-// 7. splice(): Changes the contents of an array by removing, replacing, or adding elements in-place.
-const array8 = [1, 2, 3, 4, 5];
-const removedElements = array8.splice(2, 2, 6, 7);
-console.log("splice() - array8:", array8); // [1, 2, 6, 7, 5]
-console.log("splice() - removedElements:", removedElements); // [3, 4]
+// 6. splice()
+// const array6 = [1, 2, 3, 4, 5];
+// const removedElement = array6.splice(2, 2, 6, 7);
+// console.log("array6 after splice is =" + array6);
+// console.log("removedElement is = " + removedElement);
 
-// 8. forEach(): Executes a provided function once for each array element.
-const array9 = [1, 2, 3];
-array9.forEach((element) => {
-  console.log("forEach() - element:", element);
-});
-// Output:
-// forEach() - element: 1
-// forEach() - element: 2
-// forEach() - element: 3
+// 7. sort()
+// const array7 = [7, 3, 6, 1, 9];
+// console.log("array7 sorted is now = " + array7.sort());
 
-// 9. map(): Creates a new array by performing a function on each array element.
-const array10 = [1, 2, 3];
-const mappedArray = array10.map((element) => element * 2);
-console.log("map() - mappedArray:", mappedArray); // [2, 4, 6]
+// 8. fill()
+// const array8 = [1, 2, 3, 4, 5];
+// const returnValue = array8.fill(0, 2, 4);
+// console.log("array8 after fill is now = " + array8);
+// console.log("returnValue is = " + returnValue);
 
-// 10. filter(): Creates a new array with all elements that pass a test specified by a function.
-const array11 = [1, 2, 3, 4, 5];
-const filteredArray = array11.filter((element) => element % 2 === 0);
-console.log("filter() - filteredArray:", filteredArray); // [2, 4]
+// 9. copyWithin()
+// const array9 = [1, 2, 3, 4, 5];
+// array9.copyWithin(0, 2, 4);
+// console.log("array9 after copyWithin() is =" + array9);
 
-// 11. reduce(): Applies a function against an accumulator and each element in the array to reduce it to a single value.
-const array12 = [1, 2, 3, 4, 5];
-const sum = array12.reduce(
-  (accumulator, currentValue) => accumulator + currentValue
-);
-console.log("reduce() - sum:", sum); // 15
+//Iteration
 
-// 12. reduceRight(): Similar to reduce(), but works from right to left in the array.
-const array13 = [1, 2, 3];
-const concatenatedString = array13.reduceRight(
-  (accumulator, currentValue) => accumulator + currentValue.toString(),
-  ""
-);
-console.log("reduceRight() - concatenatedString:", concatenatedString); // "321"
+// 10. forEach()
+// const array10 = [1, 2, 3, 4, 5, 6];
+// array10.forEach((element) => {
+//   console.log("array element is = " + element * 2);
+// });
 
-// 13. find(): Returns the first element in the array that satisfies a provided testing function.
-const array14 = [1, 2, 3, 4, 5];
-const foundElement = array14.find((element) => element > 3);
-console.log("find() - foundElement:", foundElement); // 4
+// 11. map()
+// const array11 = [1, 2, 3, 4, 5];
+// const mapReturned = array11.map((element) => {
+//   console.log("element is = " + element);
+//   return element * 2;
+// });
+// console.log("array11 is =" + array11);
+// console.log("mapReturned value is =" + mapReturned);
 
-// 14. findIndex(): Returns the index of the first element in the array that satisfies a provided testing function.
-const array15 = [1, 2, 3, 4, 5];
-const foundIndex = array15.findIndex((element) => element > 3);
-console.log("findIndex() - foundIndex:", foundIndex); // 3
+// 12. filter()
+// const array12 = [1, 2, 3, 4, 5];
+// const filteredArray12 = array12.filter((element) => element % 2 === 0);
+// console.log("array12 is now =" + array12);
+// console.log("filteredArray is now = " + filteredArray12);
 
-// 15. some(): Checks if at least one element in the array satisfies a provided testing function.
-const array16 = [1, 2, 3, 4, 5];
-const hasEvenNumber = array16.some((element) => element % 2 === 0);
-console.log("some() - hasEvenNumber:", hasEvenNumber); // true
+// 13. reduce()
+// const array13 = [1, 2, 3, 4, 5];
+// const sum = array13.reduce(
+//   (accumlator, currentValue) => accumlator + currentValue.toString()
+// );
+// console.log("array13 is now =" + array13);
+// console.log("sum is now =" + sum);
 
-// 16. every(): Checks if all elements in the array satisfy a provided testing function.
-const array17 = [1, 2, 3, 4, 5];
-const allEvenNumbers = array17.every((element) => element % 2 === 0);
-console.log("every() - allEvenNumbers:", allEvenNumbers); // false
+// // 14. reduceRight()
+// const array14 = [1, 2, 3, 4, 5];
+// const returnedArray = array14.reduceRight(
+//   (accumlator, currentValue) => accumlator + currentValue.toString()
+// );
+// console.log("array14 returnedArray is now =" + returnedArray);
 
-// 17. indexOf(): Returns the first index at which a given element can be found in the array.
-const array18 = [1, 2, 3, 4, 5, 3];
-const firstIndex = array18.indexOf(3);
-console.log("indexOf() - firstIndex:", firstIndex); // 2
+// 3. Retrival / Manipulation
 
-// 18. lastIndexOf(): Returns the last index at which a given element can be found in the array.
-const array19 = [1, 2, 3, 4, 5, 3];
-const lastIndex = array19.lastIndexOf(3);
-console.log("lastIndexOf() - lastIndex:", lastIndex); // 5
+// 15. find()
+// const array15 = [1, 2, 3, 4, 5];
+// const foundElement = array15.find((element) => element > 2);
+// console.log("array15 is =" + array15);
+// console.log("foundElement is = " + foundElement);
 
-// 19. includes(): Determines whether an array includes a certain element.
-const array20 = [1, 2, 3, 4, 5];
-const includesThree = array20.includes(3);
-console.log("includes() - includesThree:", includesThree); // true
+// 16. findIndex()
+// const array16 = [1, 2, 3, 4, 5];
+// const foundIndex = array16.findIndex((element) => element > 2);
+// console.log("array16 is =" + array16);
+// console.log("foundIndex is = " + foundIndex);
 
-// 20. reverse(): Reverses the order of the elements in an array in-place.
-const array21 = [1, 2, 3, 4, 5];
-array21.reverse();
-console.log("reverse() - array21:", array21); // [5, 4, 3, 2, 1]
+// 17. some()
+// const array17 = [1, 2, 3, 4, 5];
+// const hasNumber = array17.some((element) => element > 6);
+// console.log("array17 is = " + array17);
+// console.log("hasNumber is = " + hasNumber);
 
-// 21. sort(): Sorts the elements of an array in-place and returns the sorted array.
-const array22 = [3, 2, 1, 4, 5];
-array22.sort();
-console.log("sort() - array22:", array22); // [1, 2, 3, 4, 5]
+// 18. every()
+// const array18 = [1, 2, 3, 4, 5];
+// const hasEveryElementG0 = array18.every((element) => element > 0);
+// console.log("array18 is = " + array18);
+// console.log("hasEveryElementG0 is = " + hasEveryElementG0);
 
-// 22. fill(): Changes all elements in an array to a static value, from a start index to an end index.
-const array23 = [1, 2, 3, 4, 5];
-array23.fill(0, 2, 4);
-console.log("fill() - array23:", array23); // [1, 2, 0, 0, 5]
+// 19. indexOf()
+// const array19 = [1, 2, 3, 4, 5];
+// const firstIndex = array19.indexOf(6);
+// console.log("firstIndex is = " + firstIndex);
 
-// 23. copyWithin(): Copies a sequence of array elements within the array to another position.
-const array24 = [1, 2, 3, 4, 5];
-array24.copyWithin(0, 2, 4);
-console.log("copyWithin() - array24:", array24); // [3, 4, 3, 4, 5]
+// 20. lastIndexOf()
+// const array20 = [1, 2, 3, 4, 5, 1, 2, 3, 4];
+// console.log("lastIndexof3 is = " + array20.lastIndexOf(6));
 
-// 24. flat(): Creates a new array with all sub-array elements concatenated recursively up to a specified depth.
-const array25 = [1, [2, [3, [4, 5]]]];
-const flattenedArray = array25.flat(2);
-console.log("flat() - flattenedArray:", flattenedArray); // [1, 2, 3, 4, 5]
+// 21. includes()
+// const array21 = [1, 2, 3, 4, 5, 6];
+// console.log("includes7 is = " + array21.includes(7));
 
-// 25. flatMap(): Maps each element using a mapping function, then flattens the result into a new array.
-const array26 = [1, 2, 3];
-const mappedArray2 = array26.flatMap((element) => [element * 2]);
-console.log("flatMap() - mappedArray2:", mappedArray2); // [2, 4, 6]
+//Creation / Conversions
 
-// 26. from(): Creates a new, shallow-copied array from an array-like or iterable object.
-const arrayLike = "Hello";
-const newArray5 = Array.from(arrayLike);
-console.log("from() - newArray5:", newArray5); // ["H", "e", "l", "l", "o"]
+// 22. from()
+// const array22 = "Hello";
+// const newArray22 = Array.from(array22);
+// console.log("newArray22 is now =" + newArray22);
 
-// 27. of(): Creates a new array with the specified elements.
-const newArray6 = Array.of(1, 2, 3);
-console.log("of() - newArray6:", newArray6); // [1, 2, 3]
+// 23. of()
+// const array23 = Array.of(1, 2, 3);
+// console.log("array23 is now = " + array23);
 
-// 28. isArray(): Checks if a value is an array.
-console.log("isArray() - Array.isArray([1, 2, 3]):", Array.isArray([1, 2, 3])); // true
+// 24. isArray()
+// console.log("array23 is array? = " + Array.isArray(array23));
 
-// 29. toString(): Returns a string representing the specified array and its elements.
-const array27 = [1, 2, 3];
-const arrayString = array27.toString();
-console.log("toString() - arrayString:", arrayString); // "1,2,3"
+// 25. toString()
+// const array25 = [1, 2, 3, 4, 5];
+// const arrayString = array25.toString();
+// console.log("array25 is = " + array25);
+// console.log("arrayString is = " + arrayString);
 
-// 30. join(): Joins all elements of an array into a string.
-const array28 = [1, 2, 3];
-const joinedString = array28.join("-");
-console.log("join() - joinedString:", joinedString); // "1-2-3"
+// 26. join()
+// const array26 = [1, 2, 3, 4, 5, 6];
+// console.log("joinedArray is = " + array26.join("-"));
 
-// 31. length: Property that specifies the number of elements in an array (read-only).
-const array29 = [1, 2, 3];
-console.log("length - array29.length:", array29.length); // 3
+// 27. concat()
+// const array27 = [1, 3, 4];
+// const array28 = [7, 3, 2];
+// const newArray = array27.concat(array28);
+// console.log("concatenated array is now =" + newArray);
+
+//Flattening / Nesting
+
+// 28. flat()
+// const array28 = [1, [2, [3, [4, [5]]]]];
+// const flattenedArray = array28.flat(4);
+// console.log("array28 is =" + array28);
+// console.log("flattenedArray is = " + flattenedArray);
+
+// 29. flatMap()
+// const array29 = [1, 2, 3];
+// const flatMapArray = array29.flatMap((e) => [e * 2]);
+// console.log("flatMapArray is = " + flatMapArray);
+
+// 30. sort() -> descending
+const array30 = [8, 2, 5, 7, 3, 2];
+const sortedArray = array30.sort();
+console.log("sorted array is =" + sortedArray);
+const descArray = array30.sort((a, b) => b - a);
+console.log("descending sorted array is =" + descArray);
